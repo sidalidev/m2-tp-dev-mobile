eae5e0893786c27298aea3cc0128add6a8eb7eb6
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-03-18 11:56:28 +0100

    refactor(navigation): externalize component into files

--- App.js
+++ App.js
@@ -1,43 +1,9 @@
 import React from 'react'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
-import { StyleSheet, Text, View } from 'react-native'
-
-class ServicesScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Services Screen</Text>
-      </View>
-    )
-  }
-}
-class UserDetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>UserDetails Screen</Text>
-      </View>
-    )
-  }
-}
-class UsersScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Users Screen</Text>
-      </View>
-    )
-  }
-}
-class DevelopersScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Developers Screen</Text>
-      </View>
-    )
-  }
-}
+import ServicesScreen from './screens/ServicesScreen'
+import UserDetailsScreen from './screens/UserDetailsScreen'
+import UsersScreen from './screens/UsersScreen'
+import DevelopersScreen from './screens/DevelopersScreen'
 
 const AppNavigator = createStackNavigator(
   {
@@ -68,12 +34,3 @@ export default class App extends React.Component {
     return <AppContainer />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
